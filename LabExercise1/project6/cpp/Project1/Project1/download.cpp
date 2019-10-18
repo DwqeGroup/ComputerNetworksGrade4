cc1bb4998a2d0410e492fd7965bb8d236147c861
wojiaozouzixi <39880720+wojiaozouzixi@users.noreply.github.com>
2019-10-18 21:29:35 +0800

    add cpp 1/2/3/6

---
+++ LabExercise1/project6/cpp/Project1/Project1/download.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <windows.h>
+#include <urlmon.h>
+
+#pragma comment(lib, "urlmon.lib")
+
+using namespace std;
+
+BOOL FileExistsStatus(const CHAR* path)
+{
+	DWORD dwAttribute = GetFileAttributes(path);
+	if (dwAttribute == 0XFFFFFFFF) return false; //0XFFFFFFFF表示文件不存在
+	else return true;
+}
+
+BOOL DownloadFiles(const CHAR* url, const CHAR* downloadPath)
+{
+	if (URLDownloadToFile(NULL, url, downloadPath, 0, 0) == S_OK && FileExistsStatus(downloadPath)) return true;
+	else return false;
+}
+
+int main(int argc, char* argv[])
+{
+	if (DownloadFiles(argv[1], argv[2])) printf("OK!\n");
+	else printf("Error!\n");
+	getchar();
+	return 0;
+}
